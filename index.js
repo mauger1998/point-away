7b3052298779f3c3dc47ee8fdfe19fd977970da0
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-07-08 12:43:57 +0100

    Test again

--- index.js
+++ index.js
@@ -62,6 +62,8 @@ Power1.ease,
 
 })
 
+
+
 window.addEventListener("resize", () => {
 
   firstSplit.revert()
@@ -86,15 +88,7 @@ gsap.to("main .wrapper a button", {
 
 // Three Card grid text Split
 
-let textLinesH3 = document.querySelectorAll(".h3")
-let splitedUp;
 
-
-textLinesH3.forEach(textLine => {
-      splitedUp = new SplitType(textLine, {
-      type: "chars"
-  })
-})
 
 
 gsap.from(".three-card-grid .icon", {
@@ -112,70 +106,9 @@ ease:
 Power1.ease,
 
 })
-gsap.from(".three-card-grid .char", {
-  scrollTrigger: {
-    trigger: ".three-card-grid .word",
-    start: "top bottom", // when the top of the trigger hits the top of the viewport
-    scrub: 1,
-    end:"+=450px"
-  },
-yPercent: -220,
-stagger: 0.0075,
-duration:0.75,
-delay:0.3,
-ease:
-Power1.ease,
-
-})
-window.addEventListener("resize", () => {
-
-  splitedUp.revert()
-  textLinesH3.forEach(textLine => {
-    splitedUp = new SplitType(textLine, {
-    type: "chars"
-})
-
-splitedUp.revert()
-  })    
-})
-
-// Restoration Title
-
-let textLinesRestorationH2 = document.querySelectorAll(".restoration-section .restoration-h2")
-let splitted;
 
 
-textLinesRestorationH2.forEach(textLine => {
-      splited = new SplitType(textLine, {
-      type: "chars"
-  })
-})
 
-gsap.from(".restoration-section .notice-titles h2 .char", {
-  scrollTrigger: {
-    trigger: ".restoration-section .notice-titles h2 .char",
-    start: "top bottom", // when the top of the trigger hits the top of the viewport
-    scrub: 1,
-    end:"+=350px"
-  },
-yPercent: -220,
-stagger: 0.05,
-duration:0.75,
-delay:0.3,
-ease:
-Power1.ease,
-
-})
-
-window.addEventListener("resize", () => {
-  splited.revert()
-  textLinesRestorationH2.forEach(textLine => {
-    splited = new SplitType(textLine, {
-    type: "chars"
-})
-splited.revert()
-  })    
-})
 
 
 // Restoration Grid
@@ -195,43 +128,7 @@ scale:1,
 
 })
 
-// Heritage Title
 
-
-let textLinesHeritageH2 = document.querySelectorAll(".heritage h2")
-let split;
-
-split = new SplitType(textLinesHeritageH2, {
-    type: "chars"
-})
-
-
-gsap.from(".heritage h2 .char", {
-  scrollTrigger: {
-    trigger: ".heritage h2 .char",
-    start: "top bottom", // when the top of the trigger hits the top of the viewport
-    scrub: 1,
-    end:"+=400px"
-  },
-yPercent: -220,
-stagger: 0.05,
-duration:0.75,
-delay:0.3,
-ease:
-Power1.ease,
-
-})
-
-window.addEventListener("resize", () => {
-  
-    split.revert()
-    split = new SplitType(textLinesHeritageH2, {
-      type: "chars"
-  })
-    split.revert()
-
-    
-  })
 
 
 
@@ -239,19 +136,41 @@ window.addEventListener("resize", () => {
 
 //Personalised Approach Cards
 
-gsap.to(".notice .card-grid-item", {
-  scrollTrigger: {
-    trigger: ".notice .card-grid-item",
-    start: "top bottom", // when the top of the trigger hits the top of the viewport
-    scrub: 1,
-    end:"+=700px"
-  },
-x: 0,
-stagger: 0.1,
-ease:
-Power1.ease,
-scale:1,
 
+mm.add("(min-width: 801px)", () =>  {
+  // Overlap Grid Cards
+  gsap.to(".notice .card-grid-item", {
+    scrollTrigger: {
+      trigger: ".notice .card-grid-item",
+      start: "top bottom", // when the top of the trigger hits the top of the viewport
+      scrub: 1,
+      end:"+=700px"
+    },
+  x: 0,
+  stagger: 0.1,
+  ease:
+  Power1.ease,
+  scale:1,
+  
+  })
+
+})
+mm.add("(max-width: 800px)", () =>  {
+  // Overlap Grid Cards
+  gsap.to(".notice .card-grid-item", {
+    scrollTrigger: {
+      trigger: ".notice .card-grid-item",
+      start: "top bottom", // when the top of the trigger hits the top of the viewport
+      scrub: 1,
+      end:"+=500px"
+    },
+  x: 0,
+  stagger: 0.1,
+  ease:
+  Power1.ease,
+  scale:1,
+  
+  })
 })
 
 // Overlap Grid Cards
@@ -289,166 +208,16 @@ mm.add("(max-width: 800px)", () =>  {
   ease:
   Power1.ease,
   scale:1,
-
 })
-
-})
-
-// Two Column h2
-
-let twoColumnlines = document.querySelectorAll(".two-columns-section .h2")
-let twoColumnSplit;
-
-twoColumnlines.forEach(textLine => {
-  twoColumnSplit = new SplitType(textLine, {
-      type: "chars"
-  })
-  console.log(twoColumnSplit)
 })
 
 
 
 
-gsap.from(".two-columns-section .h2 .char", {
-  scrollTrigger: {
-    trigger: ".two-columns-section .h2",
-    start: "top bottom", // when the top of the trigger hits the top of the viewport
-    scrub: 1,
-    end:"+=350px"
-  },
-yPercent: -220,
-stagger: 0.05,
-duration:0.75,
-delay:0.3,
-ease:
-Power1.ease,
-
-})
-
-
-window.addEventListener("resize", () => {
-  twoColumnlines.forEach(textLine => {
-    twoColumnSplit.revert()
-    twoColumnSplit = new SplitType(textLine, {
-        type: "chars"
-    })
-    twoColumnSplit.revert()
-
-    
-  })
-})
-
-
-// Minimal Carbon
-
-let twoCarbonLines = document.querySelectorAll(".carbon-h2")
-let carbonSplit;
-
-twoCarbonLines.forEach(textLine => {
-  carbonSplit = new SplitType(textLine, {
-      type: "chars"
-  })
-})
 
 
 
 
-gsap.from(".carbon-h2 .char", {
-  scrollTrigger: {
-    trigger: ".carbon-h2 .char",
-    start: "top bottom", // when the top of the trigger hits the top of the viewport
-    scrub: 1,
-    end:"+=350px"
-  },
-yPercent: -220,
-stagger: 0.05,
-duration:0.75,
-delay:0.3,
-ease:
-Power1.ease,
-
-})
-
-window.addEventListener("resize", () => {
-  twoCarbonLines.forEach(textLine => {
-    carbonSplit.revert()
-    carbonSplit = new SplitType(textLine, {
-        type: "chars"
-    })
-    carbonSplit.revert()
-
-    
-  })
-})
-
-let sundayTextLines = document.querySelectorAll(".sunday-h2")
-let sundaySplit;
 
 
-sundayTextLines.forEach(textLine => {
-  sundaySplit = new SplitType(textLine, {
-      type: "chars"
-  })
-})
 
-gsap.from(".sunday-h2 .char", {
-  scrollTrigger: {
-    trigger: ".sunday-h2 .char",
-    start: "top bottom", // when the top of the trigger hits the top of the viewport
-    scrub: 1,
-    end:"+=350px"
-  },
-yPercent: -220,
-stagger: 0.05,
-duration:0.75,
-delay:0.3,
-ease:
-Power1.ease,
-
-})
-
-window.addEventListener("resize", () => {
-  sundaySplit.revert()
-  sundayTextLines.forEach(textLine => {
-    sundaySplit = new SplitType(textLine, {
-    type: "chars"
-})
-sundaySplit.revert()
-  })    
-})
-
-let quoteTextLines = document.querySelectorAll(".quote-h2")
-let quoteSplit;
-
-
-quoteTextLines.forEach(textLine => {
-  quoteSplit = new SplitType(textLine, {
-      type: "chars"
-  })
-})
-
-gsap.from(".quote-h2 .char", {
-  scrollTrigger: {
-    trigger: ".quote-h2 .char",
-    start: "top bottom", // when the top of the trigger hits the top of the viewport
-    scrub: 1,
-    end:"+=350px"
-  },
-yPercent: -220,
-stagger: 0.05,
-duration:0.75,
-delay:0.3,
-ease:
-Power1.ease,
-
-})
-
-window.addEventListener("resize", () => {
-  quoteSplit.revert()
-  quoteTextLines.forEach(textLine => {
-    quoteSplit = new SplitType(textLine, {
-    type: "chars"
-})
-quoteSplit.revert()
-  })    
-})
