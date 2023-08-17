fb344924cfd253270882e1e41e8e4fe79ff779d7
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-08-17 11:34:02 +0100

    Push animations to git

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 gsap.registerPlugin(ScrollTrigger);
 
+//Loading Animations
+
+const h1 = document.querySelector("h1")
+
+gsap.to(h1, {
+  clipPath:"polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+});
+gsap.to("main .wrapper p", {
+  clipPath:"polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+  delay:0.25,
+});
+gsap.to("main .wrapper button", {
+  clipPath:"polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+  delay:0.35,
+});
+
+
+
 // Dropdown
 document.addEventListener("click", (e) => {
     const isDropdownButton = e.target.matches("[data-dropdown-button]")
